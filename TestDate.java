4d9db12b024a2b9b8cdd8f4407132387953e5cc4
AdamRoux <roux.adam69@gmail.com>
2021-12-09 09:43:16 +0100

    exercice corrigé

--- TestDate.java
+++ TestDate.java
@@ -8,24 +8,22 @@ class TestDate {
 
     public static void main(String[] args) {
 
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Please fill a day (1-31) :");
-//        int day = sc.nextInt();
-//        
-//        System.out.println("Please fill a month (1-12) :");
-//        int month = sc.nextInt();
-//        System.out.println("Please fill a year :");
-//        int year = sc.nextInt();
-//        sc.close();
-   	 
-        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
-        Calendar c = Calendar.getInstance();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please fill a day (1-31) :");
+        int day = sc.nextInt();
+        
+        System.out.println("Please fill a month (1-12) :");
+        int month = sc.nextInt();
+        System.out.println("Please fill a year :");
+        int year = sc.nextInt();
+        sc.close();
+        
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyy");
+        GregorianCalendar c = new GregorianCalendar(year, month-1, day);
+        Date date = c.getTime();
         int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
-        Date d = c.getTime();
-        System.out.println("The date is : " + sdf.format(d) );
-        System.out.println("The day of week is : " + dayOfWeek );
-
-        // TODO : your code after this line
+        System.out.println("The date is : " + sdf.format(date));
+        System.out.println("The day of week is : " + dayOfWeek);
         
     }
 }
